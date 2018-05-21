df16bb4c915345f4de826de11f2907e6e47a121a
2165707 <2165707@slu.edu.ph>
2018-05-21 22:18:35 +0800

    added additional notes for ruby

--- Finals/Course Website PHPs/ruby.php
+++ Finals/Course Website PHPs/ruby.php
@@ -75,6 +75,85 @@
 					<li>Lack of Documentation</li>
 					<li>On cutting age of development - that is, some features may be 'experimental' or not yet completely supported</li>
 				</ul>
+
+				<h3>Creating a new ruby on rails</h3>
+				<p>&nbsp;&nbsp;&nbsp;&nbsp;After installing ruby on rails you can already used the command line to create your rails and then you can install the rails gem by typing the command "gem install rails" the you can create the following list below:</p>
+
+				<div class="sample-code">
+					<p>$ rails new commandsapp</p>
+					<p>&nbsp;&nbsp;create</p>
+					<p>&nbsp;&nbsp;create README.md</p>
+					<p>&nbsp;&nbsp;create Rakefile</p>
+					<p>&nbsp;&nbsp;create config.ru</p>
+					<p>&nbsp;&nbsp;create .gitignore</p>
+					<p>&nbsp;&nbsp;create Gemfile</p>
+					<p>&nbsp;&nbsp;create app</p>
+					<p>&nbsp;&nbsp;...</p>
+					<p>&nbsp;&nbsp;tmp/cache</p>
+				</div>
+
+				<h3>Rails Server</h3>
+				<p>&nbsp;&nbsp;&nbsp;&nbsp;The server for rail will launch a webserver named puma which is bundled that with rails. You can run this in the new rails app</p>
+
+				<div class="sample-code">
+					<p>$ cd commandsapp</p>
+					<p>$ bin/rails server</p>
+					<p>=> Booting Puma</p>
+					<p>=> Rails 5.1.0 application starting in development on http://0.0.0.0:3000</p>
+					<p>=> Run `rails server -h` for more startup options</p>
+					<p>Puma starting in single mode...</p>
+					<p>* Version 3.0.2 (ruby 2.3.0-p0), codename: Plethora of Penguin Pinatas</p>
+					<p>* Min threads: 5, max threads: 5</p>
+					<p>* Environment: development</p>
+					<p>* Listening on tcp://localhost:3000</p>
+					<p>User Ctrl-C to stop</p>
+				</div>
+
+				<h3>Session Handling</h3>
+				<p>&nbsp;&nbsp;&nbsp;&nbsp;Session is the most perfect place to store the data during a request that can
+be read during or later request.</p>
+				<p>&nbsp;&nbsp;&nbsp;&nbsp;Data Can Be set in action from the controller:</p>
+
+				<div class="sample-code">
+					<p>--------------------------------------</p>
+					<p>app/controller/sessions_controller.rb</p>
+					<p>--------------------------------------</p>
+					<p>def create</p>
+					<p>&nbsp;&nbsp;# ... </p>
+					<p>&nbsp;&nbsp;session[:current_user_id] = @user.id</p>
+					<p>&nbsp;&nbsp;# ... </p>
+					<p>end</p>
+				</div>
+
+				<p>Then read it in another file</p>
+				<div class="sample-code">
+					<p>--------------------------------------</p>
+					<p>app/controllers/users_controller.rb</p>
+					<p>--------------------------------------</p>
+					<p>def index</p>
+					<p>&nbsp;&nbsp;current_user = User.find_by_id(session[:current_user_id])</p>
+					<p>&nbsp;&nbsp;# ...</p>
+					<p>end</p>
+				</div>
+
+				<p>Then After you request a page, the server will set a cookie to respond back</p>
+
+				<div class="sample-code">
+					<p>~ jweiss$ curl -I http://www.google.com | grep Set-Cookie</p>
+					<p>Set-Cookie: NID=67=J2xeyegolV0SSneukSOANOCoeuDQs7G1FDAK2j-nVyaoejz-4K6aouUQtyp5B_rK3Z7G-EwTIzDm7XQ3_ZUVNnFmlGfIHMAnZQNd4kM89VLzCsM0fZnr_N8-idASAfBEdS; expires=Wed, 16-Sep-2015 05:44:42 GMT; path=/; domain=.google.com; HttpOnly</p>
+				</div>
+
+				<p>And after your cookie expires,It will make another request, and the browser
+will send again the cookie back to the server:</p>
+				
+				<div class="sample-code">
+					<p>> GET / HTTP/1.1</p>
+					<p>> User-Agent: curl/7.37.1</p>
+					<p>> Host: www.google.com</p>
+					<p>> Accept: */*</p>
+					<p>> Cookie: NID=67=J2xeyegolV0SSneukSOANOCoeuDQs7G1FDAK2j-nVyaoejz-4K6aouUQtyp5B_rK3Z7G-EwTIzDm7XQ3_ZUVNnFmlGfIHMAnZQNd4kM89VLzCsM0fZnr_N8-idASAfBEdS; expires=Wed, 16-Sep-2015 05:44:42 GMT; path=/; domain=.google.com; HttpOnly</p>
+				</div>
+
 			</div>
     </section>
 	
